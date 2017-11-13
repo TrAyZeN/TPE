952cd59b7b5b70e304459b0aa820368ff87e9e29
Hello guys <31016188+TrAyZeN@users.noreply.github.com>
2017-11-13 09:19:13 +0100

    Update tpe.py

--- tpe.py
+++ tpe.py
@@ -10,25 +10,32 @@ def nothing(x):
 trackbars = np.zeros((300, 512, 3), np.uint8)
 cv2.namedWindow("trackbars")
 
-hl = "Hue Low"
-hh = "Hue High"
-sl = "Saturation Low"
-sh = "Saturation High"
-vl = "Value Low"
-vh = "Value High"
+hlt = "Hue Low"
+hht = "Hue High"
+slt = "Saturation Low"
+sht = "Saturation High"
+vlt = "Value Low"
+vht = "Value High"
 
 # Create trackbars
-cv2.createTrackbar(hl, "trackbars", 0, 179, nothing)
-cv2.createTrackbar(hh, "trackbars", 0, 179, nothing)
-cv2.createTrackbar(sl, "trackbars", 0, 255, nothing)
-cv2.createTrackbar(sh, "trackbars", 0, 255, nothing)
-cv2.createTrackbar(vl, "trackbars", 0, 255, nothing)
-cv2.createTrackbar(vh, "trackbars", 0, 255, nothing)
+cv2.createTrackbar(hlt, "trackbars", 0, 179, nothing)
+cv2.createTrackbar(hht, "trackbars", 0, 179, nothing)
+cv2.createTrackbar(slt, "trackbars", 0, 255, nothing)
+cv2.createTrackbar(sht, "trackbars", 0, 255, nothing)
+cv2.createTrackbar(vlt, "trackbars", 0, 255, nothing)
+cv2.createTrackbar(vht, "trackbars", 0, 255, nothing)
+
+def setTb(hl, hh, sl, sh, vl, vh):
+    cv2.setTrackbarPos(hlt, "trackbars", hl)
+    cv2.setTrackbarPos(hht, "trackbars", hh)
+    cv2.setTrackbarPos(slt, "trackbars", sl)
+    cv2.setTrackbarPos(sht, "trackbars", sh)
+    cv2.setTrackbarPos(vlt, "trackbars", vl)
+    cv2.setTrackbarPos(vht, "trackbars", vh)
+
 
 # Setting hh, sh, vh to track everything on the screen
-cv2.setTrackbarPos(hh, "trackbars", 179)
-cv2.setTrackbarPos(sh, "trackbars", 255)
-cv2.setTrackbarPos(vh, "trackbars", 255)
+setTb(0, 179, 0, 255, 0, 255)
 
 while True:
     ret, frame = cap.read()
@@ -40,19 +47,33 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Get trackbars value to define hsvMin and hsvMax
-    hLow = cv2.getTrackbarPos(hl, "trackbars")
-    hHigh = cv2.getTrackbarPos(hh, "trackbars")
-    sLow = cv2.getTrackbarPos(sl, "trackbars")
-    sHigh = cv2.getTrackbarPos(sh, "trackbars")
-    vLow = cv2.getTrackbarPos(vl, "trackbars")
-    vHigh = cv2.getTrackbarPos(vh, "trackbars")
+    hLow = cv2.getTrackbarPos(hlt, "trackbars")
+    hHigh = cv2.getTrackbarPos(hht, "trackbars")
+    sLow = cv2.getTrackbarPos(slt, "trackbars")
+    sHigh = cv2.getTrackbarPos(sht, "trackbars")
+    vLow = cv2.getTrackbarPos(vlt, "trackbars")
+    vHigh = cv2.getTrackbarPos(vht, "trackbars")
 
     # lower and upper range to track
     hsvMin = np.array([hLow,sLow,vLow])
     hsvMax = np.array([hHigh,sHigh,vHigh])
 
     mask = cv2.inRange(hsv,hsvMin, hsvMax)
-    result = cv2.bitwise_and(frame,frame, mask = mask)
+    bmask = cv2.GaussianBlur(mask, (5,5),0)
+    result = cv2.bitwise_and(frame,frame, mask = bmask)
+
+    # -- CONTOURS --
+    ret, thresh = cv2.threshold(bmask, 127, 255, 0)
+    contours, _ = cv2.findContours(thresh, 1, 2)
+
+    if len(contours) > 0:       # Prevent from error
+        cnt = contours[-1]
+        x,y,w,h = cv2.boundingRect(cnt)  #IndexError: list index out of range
+        if w > 40 and h > 40:
+            print x,y,w,h
+            cv2.rectangle(result,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.circle(result, (x+w/2,y+h/2), 2, (0,0,255), 2)
+
 
     # Display every windows
     cv2.imshow("original", frame)
@@ -65,12 +86,10 @@ while True:
     if k == 27:             # Press ESC key to quit
         break
     elif k == ord('b'):     # Shortcut to track my blue handspinner :D
-        cv2.setTrackbarPos(hl, "trackbars", 106)
-        cv2.setTrackbarPos(hh, "trackbars", 179)
-        cv2.setTrackbarPos(sl, "trackbars", 98)
-        cv2.setTrackbarPos(sh, "trackbars", 174)
-        cv2.setTrackbarPos(vl, "trackbars", 37)
-        cv2.setTrackbarPos(vh, "trackbars", 200)
+        setTb(106, 179, 98, 174, 37, 200)
+
+    elif k == ord('r'):     # Shortcut to reset trackbars
+        setTb(0, 179, 0, 255, 0, 255)
 
 cap.release()
 cv2.destroyAllWindows()
