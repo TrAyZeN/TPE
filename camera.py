2caf9a02ec9f1f9a1fb47a8737e2527109bcc093
TrAyZeN <31016188+TrAyZeN@users.noreply.github.com>
2018-06-03 18:47:00 +0200

    update to PEP 8
    update variable, function and class name to PEP 8

--- camera.py
+++ camera.py
@@ -8,112 +8,105 @@ import math
 from main import *
 
 # Initialize global variables
-fpsMax = 30                 # maximum frame per second of the app
-focalLength = 731.77        # focal length of my camera in pixel
-knownWidth = 0.125          # my object width in meter
-vMax = 2                    # maximum speed authorized
-hLow = 0
-hHigh = 179
-sLow = 0
-sHigh = 255
-vLow = 0
-vHigh = 255
-xPrev = 0
-yPrev = 0
-wPrev = 0
-hPrev = 0
-xCenterPrev = 0
-yCenterPrev = 0
-zPrev = 0
-startTimePrev = 0
+fps = 30                        # refresh rate of the app
+FOCAL_LENGTH = 731.77           # focal length of my camera in pixel
+OBJECT_WIDTH = 0.125            # my object width in meter
+MAX_SPEED = 2                   # maximum speed authorized
+hue_low = 0
+hue_high = 179
+saturation_low = 0
+saturation_high = 255
+value_low = 0
+value_high = 255
+prev_x = 0
+prev_y = 0
+prev_w = 0
+prev_h = 0
+prev_x_center = 0
+prev_y_center = 0
+prev_z = 0
+prev_start_time = 0
 selection = 0
-contourSelection = 0
-contourRotatingSelection = 0
+contour_selection = 0
+rotating_contour_selection = 0
+x = 0
+y = 0
+w = 0
+h = 0
 
 class Camera(Image):
 
     def __init__(self, **kwargs):
         super(Camera, self).__init__(**kwargs)
         self.capture = cv2.VideoCapture(0)
-        fps = 30
         Clock.schedule_interval(self.update, 1.0 / fps)
 
     def update(self, dt):
-        global fpsMax, hLow, sLow, vLow, hHigh, sHigh, vHigh, xPrev, yPrev, wPrev, hPrev, xCenterPrev, yCenterPrev, zPrev, startTimePrev, selection, contourSelection, contourRotatingSelection
+        global hue_low, saturation_low, value_low, hue_high, saturation_high,\
+            value_high,prev_x, prev_y, prev_w, prev_h, prev_x_center,\
+            prev_y_center, prev_z, prev_start_time, x, y, w, h
 
         ret, frame = self.capture.read()
         self.size = (frame.shape[1], frame.shape[0])
-        startTime = time.time()
-        deltaTime = startTime - startTimePrev # time between the frame and the previous frame
+        start_time = time.time()
+        delta_time = start_time - prev_start_time # time between the frame and the previous frame
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         # lower and upper range to track
-        hsvMin = np.array([hLow,sLow,vLow])
-        hsvMax = np.array([hHigh,sHigh,vHigh])
+        hsv_min = np.array([hue_low,saturation_low,value_low])
+        hsv_max = np.array([hue_high,saturation_high,value_high])
 
-        mask = cv2.inRange(hsv,hsvMin, hsvMax)
+        mask = cv2.inRange(hsv,hsv_min, hsv_max)
         bmask = cv2.GaussianBlur(mask, (5,5),0)
 
         # Contours
         ret, thresh = cv2.threshold(bmask, 127, 255, 0)
-        contours, _ = cv2.findContours(thresh, 1, 2)
+        _, contours, _ = cv2.findContours(thresh, 1, 2)
 
         if len(contours) > 0:       # Prevent from error when there is no contour
-            cnt = contours[-1]
-            x,y,w,h = cv2.boundingRect(cnt)
-            xCenter = x+w/2
-            yCenter = y+h/2
+            self.cnt = contours[-1]
+            x, y, w, h = cv2.boundingRect(self.cnt)
+            self.xCenter = x + w/2
+            self.yCenter = y + h/2
 
             # calculate distance between the camera and the object
             if w > h:
-                z = (knownWidth * focalLength) / w
+                z = (OBJECT_WIDTH*FOCAL_LENGTH) / w
             else:
-                z = (knownWidth * focalLength) / h
+                z = (OBJECT_WIDTH*FOCAL_LENGTH) / h
 
             # calculate speed
-            vector = [abs(xCenterPrev - xCenter), abs(yCenterPrev - yCenter), abs(zPrev - z)]
-            vector.append(math.sqrt(vector[0] ** 2 + vector[1] ** 2))
-            vector.append(round(math.sqrt(vector[2] ** 2 + vector[3] ** 2), 2))
+            vector = [abs(prev_x_center - self.xCenter), abs(prev_y_center - self.yCenter), abs(prev_z - z)]
+            vector.append(math.sqrt(vector[0]**2 + vector[1]**2))
+            vector.append(round(math.sqrt(vector[2]**2 + vector[3]**2), 2))
 
-            scale = (z * vector[3]) / focalLength
-            v = scale / deltaTime
-            vKm = round(v * 3.6, 2)
-            #print("{0} km/h".format(vKm))
+            scale = (z*vector[3]) / FOCAL_LENGTH
+            v = scale / delta_time
+            speed_km = round(v * 3.6, 2)
+            #print("{0} km/h".format(speed_km))
 
-            if vKm > vMax:
-                print("Slow down")
+            if speed_km > MAX_SPEED:
+                print("slow down")
                 if arduino == True:
-                    s.write("Slow down")
+                    s.write("slow down")
 
-            elif vKm > 0.5 and vKm < vMax:
+            elif speed_km > 0.5 and speed_km < MAX_SPEED:
                 print("Good speed")
                 if arduino == True:
                     s.write("Good speed")
 
-            xPrev = x
-            yPrev = y
-            wPrev = w
-            hPrev = h
-            xCenterPrev = xCenter
-            yCenterPrev = yCenter
-            zPrev = z
-            startTimePrev = startTime
+            prev_x = x
+            prev_y = y
+            prev_w = w
+            prev_h = h
+            prev_x_center = self.xCenter
+            prev_y_center = self.yCenter
+            prev_z = z
+            prev_start_time = start_time
 
         if selection == 0:
-            if len(contours) > 0:
-                if w > 40 and h > 40:
-                    if contourSelection == 1:
-                        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-                        # draw the center of the recangle
-                        cv2.circle(frame, (xCenter, yCenter), 2, (0,255,0), 2)
-                    if contourRotatingSelection == 1:
-                        # rotated rectangle
-                        rect = cv2.minAreaRect(cnt)
-                        box = cv2.cv.BoxPoints(rect)
-                        box = np.int0(box)
-                        cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
-
+            self.draw_contours(contours, frame)      # variable x referenced before assignement
             buf1 = cv2.flip(frame, 0)
 
         elif selection == 1:
@@ -121,37 +114,13 @@ class Camera(Image):
             edged = cv2.Canny(gray, 50, 100)
             bgrEdged = cv2.cvtColor(edged, cv2.COLOR_GRAY2BGR) # convert a single channel image to a 3 channels image
 
-            if len(contours) > 0:
-                if w > 40 and h > 40:
-                    if contourSelection == 1:
-                        cv2.rectangle(bgrEdged,(x,y),(x+w,y+h),(0,255,0),2)
-                        # draw the center of the recangle
-                        cv2.circle(bgrEdged, (xCenter, yCenter), 2, (0,255,0), 2)
-                    if contourRotatingSelection == 1:
-                        # rotated rectangle
-                        rect = cv2.minAreaRect(cnt)
-                        box = cv2.cv.BoxPoints(rect)
-                        box = np.int0(box)
-                        cv2.drawContours(bgrEdged, [box], 0, (0, 0, 255), 2)
-
+            self.draw_contours(contours, bgrEdged)
             buf1 = cv2.flip(bgrEdged, 0)
 
         elif selection == 2:
             bgrMask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) # convert a single channel image to a 3 channels image
 
-            if len(contours) > 0:
-                if w > 40 and h > 40:
-                    if contourSelection == 1:
-                        cv2.rectangle(bgrMask,(x,y),(x+w,y+h),(0,255,0),2)
-                        # draw the center of the recangle
-                        cv2.circle(bgrMask, (xCenter, yCenter), 2, (0,255,0), 2)
-                    if contourRotatingSelection == 1:
-                        # rotated rectangle
-                        rect = cv2.minAreaRect(cnt)
-                        box = cv2.cv.BoxPoints(rect)
-                        box = np.int0(box)
-                        cv2.drawContours(bgrMask, [box], 0, (0, 0, 255), 2)
-
+            self.draw_contours(contours, bgrMask)
             buf1 = cv2.flip(bgrMask, 0)
 
         else:
@@ -163,3 +132,18 @@ class Camera(Image):
         image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         # display image from the texture
         self.texture = image_texture
+
+    def draw_contours(self, contours, frame):
+        if len(contours) > 0:
+            #global x, y, w, h
+            if w > 40 and h > 40:
+                if contour_selection == 1:
+                    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+                    # draw the center of the recangle
+                    cv2.circle(frame, (self.xCenter, self.yCenter), 2, (0,255,0), 2)
+                if rotating_contour_selection == 1:
+                    # rotated rectangle
+                    rect = cv2.minAreaRect(self.cnt)
+                    box = cv2.boxPoints(rect) # cv.
+                    box = np.int0(box)
+                    cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
