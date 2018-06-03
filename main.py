68bf9abed3fe645486d248369133702e451a758b
TrAyZeN <31016188+TrAyZeN@users.noreply.github.com>
2018-06-03 18:48:10 +0200

    Update to PEP 8
    Update variable, function and class name to PEP 8

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ Config.set('graphics', 'resizable', '0')
 arduino = False             # Arduino is initialized or not
 
 try:
-    s = serial.Serial(port='COM4', baudrate=115200)
+    s = serial.Serial(port='COM4', baudrate=115200)     # initialize the connection with arduino
 except serial.serialutil.SerialException:
     print("could not open port 'COM4'")
 else:
@@ -48,190 +48,189 @@ class RootWidget(Widget):
         self.cam = camera.Camera(pos=(20, 268))
         self.add_widget(self.cam)
 
-        def updateBut():
+        def update_but():
             if camera.selection != 0:
-                butFrameSkin.unselect()
+                frame_button_skin.unselect()
             if camera.selection != 1:
-                butEdgeSkin.unselect()
+                canny_button_skin.unselect()
             if camera.selection != 2:
-                butMaskSkin.unselect()
+                mask_button_skin.unselect()
 
         # button frame
-        def pressButFrame(instance):
+        def press_frame_button(instance):
             camera.selection = 0
-            butFrameSkin.select()
-            updateBut()
+            frame_button_skin.select()
+            update_but()
 
-        butFrame = Button(pos=(40, 200), size=(40, 40))
-        butFrame.bind(on_press=pressButFrame)
-        labFrame = Label(text='Frame', pos=(74, 170), font_name='calibri', font_size=20)
-        butFrameSkin = buttonSkin(pos=butFrame.pos, size=butFrame.size)
-        butFrameSkin.select()
+        frame_button = Button(pos=(40, 200), size=(40, 40))
+        frame_button.bind(on_press=press_frame_button)
+        frame_label = Label(text='Frame', pos=(74, 170), font_name='calibri', font_size=20)
+        frame_button_skin = ButtonSkin(pos=frame_button.pos, size=frame_button.size)
+        frame_button_skin.select()
 
         # button canny edge
-        def pressButEdge(instance):
+        def press_canny_button(instance):
             camera.selection = 1
-            butEdgeSkin.select()
-            updateBut()
+            canny_button_skin.select()
+            update_but()
 
-        butEdge = Button(pos=(40, 150), size=(40, 40))
-        butEdge.bind(on_press=pressButEdge)
-        labEdge = Label(text='Canny edge', pos=(97, 120), font_name='calibri', font_size=20)
-        butEdgeSkin = buttonSkin(pos=butEdge.pos, size=butEdge.size)
+        canny_button = Button(pos=(40, 150), size=(40, 40))
+        canny_button.bind(on_press=press_canny_button)
+        canny_label = Label(text='Canny edge', pos=(97, 120), font_name='calibri', font_size=20)
+        canny_button_skin = ButtonSkin(pos=canny_button.pos, size=canny_button.size)
 
         # button mask
-        def pressButMask(instance):
+        def press_mask_button(instance):
             camera.selection = 2
-            butMaskSkin.select()
-            updateBut()
+            mask_button_skin.select()
+            update_but()
 
-        butMask = Button(pos=(40, 100), size=(40, 40))
-        butMask.bind(on_press=pressButMask)
-        labMask = Label(text='Mask', pos=(70, 70), font_name='calibri', font_size=20)
-        butMaskSkin = buttonSkin(pos=butMask.pos, size=butMask.size)
+        mask_button = Button(pos=(40, 100), size=(40, 40))
+        mask_button.bind(on_press=press_mask_button)
+        mask_label = Label(text='Mask', pos=(70, 70), font_name='calibri', font_size=20)
+        mask_button_skin = ButtonSkin(pos=mask_button.pos, size=mask_button.size)
 
         # button contour
-        def pressButCnt(instance):
-            if camera.contourSelection == 0:
-                camera.contourSelection = 1
-                butCntSkin.select()
+        def press_contour_button(instance):
+            if camera.contour_selection == 0:
+                camera.contour_selection = 1
+                contour_button_skin.select()
             else:
-                camera.contourSelection = 0
-                butCntSkin.unselect()
+                camera.contour_selection = 0
+                contour_button_skin.unselect()
 
-        butCnt = Button(pos=(240, 200), size=(40, 40))
-        butCnt.bind(on_press=pressButCnt)
-        labCnt = Label(text='Contour', pos=(280, 170), font_name='calibri', font_size=20)
-        butCntSkin = buttonSkin(pos=butCnt.pos, size=butCnt.size)
+        contour_button = Button(pos=(240, 200), size=(40, 40))
+        contour_button.bind(on_press=press_contour_button)
+        contour_label = Label(text='Contour', pos=(280, 170), font_name='calibri', font_size=20)
+        contour_button_skin = ButtonSkin(pos=contour_button.pos, size=contour_button.size)
 
         # button rotating contour
-        def pressButRotCnt(instance):
-            if camera.contourRotatingSelection == 0:
-                camera.contourRotatingSelection = 1
-                butRotCntSkin.select()
+        def press_rotating_countour_button(instance):
+            if camera.rotating_contour_selection == 0:
+                camera.rotating_contour_selection = 1
+                rotating_countour_button_skin.select()
             else:
-                camera.contourRotatingSelection = 0
-                butRotCntSkin.unselect()
+                camera.rotating_contour_selection = 0
+                rotating_countour_button_skin.unselect()
 
-        butRotCnt = Button(pos=(240, 150), size=(40, 40))
-        butRotCnt.bind(on_press=pressButRotCnt)
-        labRotCnt = Label(text='Rotating Contour', pos=(320, 120), font_name='calibri', font_size=20)
-        butRotCntSkin = buttonSkin(pos=butRotCnt.pos, size=butRotCnt.size)
+        rotating_countour_button = Button(pos=(240, 150), size=(40, 40))
+        rotating_countour_button.bind(on_press=press_rotating_countour_button)
+        rotating_countour_label = Label(text='Rotating Contour', pos=(320, 120), font_name='calibri', font_size=20)
+        rotating_countour_button_skin = ButtonSkin(pos=rotating_countour_button.pos, size=rotating_countour_button.size)
 
         # slider hue low
-        def sliderHlMove(instance, value):
-            if value > sliderHh.value:
-                sliderHl.value = sliderHh.value
-            camera.hLow = int(sliderHl.value)
-            labNumHl.text = str(camera.hLow)
+        def move_hue_low_slider(instance, value):
+            if value > hue_high_slider.value:
+                hue_low_slider.value = hue_high_slider.value
+            camera.hue_low = int(hue_low_slider.value)
+            hue_low_num_label.text = str(camera.hue_low)
 
-        sliderHl = Slider(range=(0,179), value=0, pos=(680,640), size=(200,100), sensitivity='handle')
-        sliderHl.bind(value=sliderHlMove)
+        hue_low_slider = Slider(range=(0,179), value=0, pos=(680,640), size=(200,100), sensitivity='handle')
+        hue_low_slider.bind(value=move_hue_low_slider)
 
         # slider hue high
-        def sliderHhMove(instance, value):
-            if value < sliderHl.value:
-                sliderHh.value = sliderHl.value
-            camera.hHigh = int(sliderHh.value)
-            labNumHh.text = str(camera.hHigh)
+        def move_hue_high_slider(instance, value):
+            if value < hue_low_slider.value:
+                hue_high_slider.value = hue_low_slider.value
+            camera.hue_high = int(hue_high_slider.value)
+            hue_high_num_label.text = str(camera.hue_high)
 
-        sliderHh = Slider(range=(0,179), value=179, pos=(680,580), size=(200,100), sensitivity='handle')
-        sliderHh.bind(value=sliderHhMove)
+        hue_high_slider = Slider(range=(0,179), value=179, pos=(680,580), size=(200,100), sensitivity='handle')
+        hue_high_slider.bind(value=move_hue_high_slider)
 
         # slider saturation low
-        def sliderSlMove(instance, value):
-            if value > sliderSh.value:
-                sliderSl.value = sliderSh.value
-            camera.sLow = int(sliderSl.value)
-            labNumSl.text = str(camera.sLow)
+        def move_saturation_low_slider(instance, value):
+            if value > saturation_high_slider.value:
+                saturation_low_slider.value = saturation_high_slider.value
+            camera.saturation_low = int(saturation_low_slider.value)
+            saturation_low_num_label.text = str(camera.saturation_low)
 
-        sliderSl = Slider(range=(0,255), value=0, pos=(680,520), size=(200,100), sensitivity='handle')
-        sliderSl.bind(value=sliderSlMove)
+        saturation_low_slider = Slider(range=(0,255), value=0, pos=(680,520), size=(200,100), sensitivity='handle')
+        saturation_low_slider.bind(value=move_saturation_low_slider)
 
         # slider saturation high
-        def sliderShMove(instance, value):
-            if value < sliderSl.value:
-                sliderSh.value = sliderSl.value
-            camera.sHigh = int(sliderSh.value)
-            labNumSh.text = str(camera.sHigh)
+        def move_saturation_high_slider(instance, value):
+            if value < saturation_low_slider.value:
+                saturation_high_slider.value = saturation_low_slider.value
+            camera.saturation_high = int(saturation_high_slider.value)
+            saturation_high_num_label.text = str(camera.saturation_high)
 
-        sliderSh = Slider(range=(0,255), value=255, pos=(680,460), size=(200,100), sensitivity='handle')
-        sliderSh.bind(value=sliderShMove)
+        saturation_high_slider = Slider(range=(0,255), value=255, pos=(680,460), size=(200,100), sensitivity='handle')
+        saturation_high_slider.bind(value=move_saturation_high_slider)
 
         # slider value low
-        def sliderVlMove(instance, value):
-            if value > sliderVh.value:
-                sliderVl.value = sliderVh.value
-            camera.vLow = int(sliderVl.value)
-            labNumVl.text = str(camera.vLow)
+        def move_value_low_slider(instance, value):
+            if value > value_high_slider.value:
+                value_low_slider.value = value_high_slider.value
+            camera.value_low = int(value_low_slider.value)
+            value_low_num_label.text = str(camera.value_low)
 
-        sliderVl = Slider(range=(0,255), value=0, pos=(680,400), size=(200,100), sensitivity='handle')
-        sliderVl.bind(value=sliderVlMove)
+        value_low_slider = Slider(range=(0,255), value=0, pos=(680,400), size=(200,100), sensitivity='handle')
+        value_low_slider.bind(value=move_value_low_slider)
 
         # slider value high
-        def sliderVhMove(instance, value):
-            if value < sliderVl.value:
-                sliderVh.value = sliderVl.value
-            camera.vHigh = int(sliderVh.value)
-            labNumVh.text = str(camera.vHigh)
+        def value_high_sliderMove(instance, value):
+            if value < value_low_slider.value:
+                value_high_slider.value = value_low_slider.value
+            camera.value_high = int(value_high_slider.value)
+            value_high_num_label.text = str(camera.value_high)
 
-        sliderVh = Slider(range=(0,255), value=255, pos=(680,340), size=(200,100), sensitivity='handle')
-        sliderVh.bind(value=sliderVhMove)
+        value_high_slider = Slider(range=(0,255), value=255, pos=(680,340), size=(200,100), sensitivity='handle')
+        value_high_slider.bind(value=value_high_sliderMove)
 
-        labSliderHl = Label(text='Hue Low', pos=(900, 640), font_name='calibri', font_size=20)
-        labSliderHh = Label(text='Hue High', pos=(900, 580), font_name='calibri', font_size=20)
-        labSliderSl = Label(text='Saturation Low', pos=(905, 520), font_name='calibri', font_size=20)
-        labSliderSh = Label(text='Saturation High', pos=(905, 460), font_name='calibri', font_size=20)
-        labSliderVl = Label(text='Value Low', pos=(900, 400), font_name='calibri', font_size=20)
-        labSliderVh = Label(text='Value High', pos=(900, 340), font_name='calibri', font_size=20)
-        labNumHl = Label(text=str(int(sliderHl.value)), pos=(655, 665), font_name='calibri', font_size=20)
-        labNumHh = Label(text=str(int(sliderHh.value)), pos=(655, 605), font_name='calibri', font_size=20)
-        labNumSl = Label(text=str(int(sliderSl.value)), pos=(655, 545), font_name='calibri', font_size=20)
-        labNumSh = Label(text=str(int(sliderSh.value)), pos=(655, 485), font_name='calibri', font_size=20)
-        labNumVl = Label(text=str(int(sliderVl.value)), pos=(655, 425), font_name='calibri', font_size=20)
-        labNumVh = Label(text=str(int(sliderVh.value)), pos=(655, 365), font_name='calibri', font_size=20)
+        hue_low_label = Label(text='Hue Low', pos=(900, 640), font_name='calibri', font_size=20)
+        hue_high_label = Label(text='Hue High', pos=(900, 580), font_name='calibri', font_size=20)
+        saturation_low_label = Label(text='Saturation Low', pos=(905, 520), font_name='calibri', font_size=20)
+        saturation_high_slider_label = Label(text='Saturation High', pos=(905, 460), font_name='calibri', font_size=20)
+        value_low_label = Label(text='Value Low', pos=(900, 400), font_name='calibri', font_size=20)
+        value_high_label = Label(text='Value High', pos=(900, 340), font_name='calibri', font_size=20)
+        hue_low_num_label = Label(text=str(int(hue_low_slider.value)), pos=(655, 665), font_name='calibri', font_size=20)
+        hue_high_num_label = Label(text=str(int(hue_high_slider.value)), pos=(655, 605), font_name='calibri', font_size=20)
+        saturation_low_num_label = Label(text=str(int(saturation_low_slider.value)), pos=(655, 545), font_name='calibri', font_size=20)
+        saturation_high_num_label = Label(text=str(int(saturation_high_slider.value)), pos=(655, 485), font_name='calibri', font_size=20)
+        value_low_num_label = Label(text=str(int(value_low_slider.value)), pos=(655, 425), font_name='calibri', font_size=20)
+        value_high_num_label = Label(text=str(int(value_high_slider.value)), pos=(655, 365), font_name='calibri', font_size=20)
 
-        self.add_widget(butFrame)
-        self.add_widget(labFrame)
-        self.add_widget(butEdge)
-        self.add_widget(labEdge)
-        self.add_widget(butMask)
-        self.add_widget(labMask)
-        self.add_widget(sliderHl)
-        self.add_widget(sliderHh)
-        self.add_widget(sliderSl)
-        self.add_widget(sliderSh)
-        self.add_widget(sliderVl)
-        self.add_widget(sliderVh)
-        self.add_widget(labSliderHl)
-        self.add_widget(labSliderHh)
-        self.add_widget(labSliderSl)
-        self.add_widget(labSliderSh)
-        self.add_widget(labSliderVl)
-        self.add_widget(labSliderVh)
-        self.add_widget(butFrameSkin)
-        self.add_widget(butEdgeSkin)
-        self.add_widget(butMaskSkin)
-        self.add_widget(labNumHl)
-        self.add_widget(labNumHh)
-        self.add_widget(labNumSl)
-        self.add_widget(labNumSh)
-        self.add_widget(labNumVl)
-        self.add_widget(labNumVh)
-        self.add_widget(butCnt)
-        self.add_widget(butCntSkin)
-        self.add_widget(labCnt)
-        self.add_widget(butRotCnt)
-        self.add_widget(butRotCntSkin)
-        self.add_widget(labRotCnt)
+        self.add_widget(frame_button)
+        self.add_widget(frame_label)
+        self.add_widget(canny_button)
+        self.add_widget(canny_label)
+        self.add_widget(mask_button)
+        self.add_widget(mask_label)
+        self.add_widget(hue_low_slider)
+        self.add_widget(hue_high_slider)
+        self.add_widget(saturation_low_slider)
+        self.add_widget(saturation_high_slider)
+        self.add_widget(value_low_slider)
+        self.add_widget(value_high_slider)
+        self.add_widget(hue_low_label)
+        self.add_widget(hue_high_label)
+        self.add_widget(saturation_low_label)
+        self.add_widget(saturation_high_slider_label)
+        self.add_widget(value_low_label)
+        self.add_widget(value_high_label)
+        self.add_widget(frame_button_skin)
+        self.add_widget(canny_button_skin)
+        self.add_widget(mask_button_skin)
+        self.add_widget(hue_low_num_label)
+        self.add_widget(hue_high_num_label)
+        self.add_widget(saturation_low_num_label)
+        self.add_widget(saturation_high_num_label)
+        self.add_widget(value_low_num_label)
+        self.add_widget(value_high_num_label)
+        self.add_widget(contour_button)
+        self.add_widget(contour_button_skin)
+        self.add_widget(contour_label)
+        self.add_widget(rotating_countour_button)
+        self.add_widget(rotating_countour_button_skin)
+        self.add_widget(rotating_countour_label)
 
 
-class buttonSkin(Image):
+class ButtonSkin(Image):
 
     def __init__(self, **kwargs):
-        super(buttonSkin, self).__init__(**kwargs)
-
-        img = cv2.imread("../resources/buttonSkin.png")
+        super(ButtonSkin, self).__init__(**kwargs)
+        img = cv2.imread("resources/buttonSkin.png")
 
         buf1 = cv2.flip(img, 0)
         buf = buf1.tostring()
@@ -259,7 +258,7 @@ class buttonSkin(Image):
         image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         # display image from the texture
         self.texture = image_texture
-        
+
 
 class TpeApp(App):
 
